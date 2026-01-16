6524909b69fdb42efb30732eb8de67efd2e63097
youssefsafwat234 <yousefsafwat367@gmail.com>
2026-01-17 01:11:09 +0200

    Refactor vendor filter view for improved readability and consistency. Adjusted HTML structure and formatting in the vendor search input and vendor route logic for better clarity.

--- resources/themes/default/web-views/products/partials/_filter-product-vendors.blade.php
+++ resources/themes/default/web-views/products/partials/_filter-product-vendors.blade.php
@@ -3,8 +3,8 @@
     <div class="pb-2">
         <div class="input-group-overlay input-group-sm">
             <input placeholder="{{ translate('search_by_vendors') }}"
-                   class="__inline-38 cz-filter-search form-control form-control-sm appended-form-control search-product-attribute"
-                   type="text">
+                class="__inline-38 cz-filter-search form-control form-control-sm appended-form-control search-product-attribute"
+                type="text">
             <div class="input-group-append-overlay">
                 <span class="input-group-text">
                     <i class="czi-search"></i>
@@ -12,20 +12,20 @@
             </div>
         </div>
     </div>
-    <ul class="__brands-cate-wrap attribute-list" data-simplebar
-        data-simplebar-auto-hide="false">
+    <ul class="__brands-cate-wrap attribute-list" data-simplebar data-simplebar-auto-hide="false">
         <div class="no-data-found text-muted" style="display:none;">{{ translate('No_Data_Found') }}</div>
-        @foreach($productVendors as $vendor)
+        @foreach ($productVendors as $vendor)
             <?php
-                if (isset($dataFrom) && $dataFrom == 'shop-view' && isset($shopSlug)) {
-                    $vendorRoute = route('shopView', ['slug' => $shopSlug, 'shop_id' => $vendor['id'],'data_from' => 'shop', 'offer_type' => ($data['offer_type'] ?? ''), 'page' => 1]);
-                } else if (isset($dataFrom) && $dataFrom == 'flash-deals') {
-                    $vendorRoute = route('flash-deals', ['id' => ($web_config['flash_deals']['id'] ?? 0), 'shop_id' => $vendor['id'],'data_from'=>'shop', 'offer_type' => ($data['offer_type'] ?? ''), 'page' => 1]);
-                } else {
-                    $vendorRoute = route('products', ['shop_id' => $vendor['id'], 'data_from'=>'shop', 'offer_type' => ($data['offer_type'] ?? ''), 'page' => 1]);
-                }
+            if (isset($dataFrom) && $dataFrom == 'shop-view' && isset($shopSlug)) {
+                $vendorRoute = route('shopView', ['slug' => $shopSlug, 'shop_id' => $vendor['id'], 'data_from' => 'shop', 'offer_type' => $data['offer_type'] ?? '', 'page' => 1]);
+            } elseif (isset($dataFrom) && $dataFrom == 'flash-deals') {
+                $vendorRoute = route('flash-deals', ['id' => $web_config['flash_deals']['id'] ?? 0, 'shop_id' => $vendor['id'], 'data_from' => 'shop', 'offer_type' => $data['offer_type'] ?? '', 'page' => 1]);
+            } else {
+                $vendorRoute = route('products', ['shop_id' => $vendor['id'], 'data_from' => 'shop', 'offer_type' => $data['offer_type'] ?? '', 'page' => 1]);
+            }
             ?>
-            <ul class="brand mt-2 p-0 for-brand-hover {{ session('direction') === "rtl" ? 'mr-2' : ''}}" id="vendor">
+            <ul class="brand mt-2 p-0 for-brand-hover {{ session('direction') === 'rtl' ? 'mr-2' : '' }}"
+                id="vendor">
                 <li class="flex-between get-view-by-onclick cursor-pointer {{ request('shop_id') == $vendor['id'] ? 'text-primary' : '' }}"
                     data-link="{{ $vendorRoute }}">
                     <div class="text-start">
@@ -41,4 +41,3 @@
         @endforeach
     </ul>
 </div>
-
